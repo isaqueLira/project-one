d5348bffedd1ea26b917830c08cd39c2e6b0beda
isaqueLira <isaque.souza.lira@gmail.com>
2022-06-05 22:31:28 -0300

    Inclusão do Projeto.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # project-one
+
+O projeto consta em um site responsivo de geração de historias no modelo de linha do tempo.
+A linha do tempo é clicável.
---
+++ scripts.js
@@ -0,0 +1,48 @@
+function show1(){
+
+    let modal = document.querySelector('.content1')
+
+    modal.style.visibility = 'visible'; 
+}
+
+function show2(){
+
+    let modal = document.querySelector('.content2')
+
+    modal.style.visibility = 'visible'; 
+}
+
+function show3(){
+
+    let modal = document.querySelector('.content3')
+
+    modal.style.visibility = 'visible'; 
+}
+
+function show4(){
+
+    let modal = document.querySelector('.content4')
+
+    modal.style.visibility = 'visible'; 
+}
+
+function show5(){
+
+    let modal = document.querySelector('.content5')
+
+    modal.style.visibility = 'visible'; 
+}
+
+function show6(){
+
+    let modal = document.querySelector('.content6')
+
+    modal.style.visibility = 'visible'; 
+}
+
+function show7(){
+
+    let modal = document.querySelector('.content7')
+
+    modal.style.visibility = 'visible'; 
+}
